24fd3b0fbbfcf18808a680dcde54a6e468f28b2f
SimoneScapin <simonescap87@gmail.com>
2022-03-24 16:30:41 +0100

    done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue(
+    {
+        el: '#data',
+        data: {
+            emailList: [],
+        },
+        methods: {    
+        },
+        mounted: function() {
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result)=> {
+                    this.email = result.data.response;
+                    this.emailList.push(this.email);
+                });
+            }
+            
+        },
+    }
+);
